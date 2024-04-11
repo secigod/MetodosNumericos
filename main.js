27e66a322232de9d4b40e0b7f3ae5e4550fa998d
secigod <82354208+secigod@users.noreply.github.com>
2024-04-10 20:33:03 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 //al iniciar selecciona inicio
-init('INCIO');
+init("INCIO");
+var countMenu = 0;
 
-function init(incio){
+
+function init(incio) {
+  document.getElementById("solution").style.display = "none";
+
   var i, x, tablinks, doc;
   x = document.getElementsByClassName("city");
   for (i = 0; i < x.length; i++) {
@@ -13,6 +17,38 @@ function init(incio){
 }
 
 function openCity(evt, cityName) {
+  document.getElementById("solution").style.display = "none";
+
+  var i, x, tablinks;
+  x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-border-yellow", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " w3-border-yellow";
+}
+
+function openMenu() {
+  document.getElementById("solution").style.display = "none";
+
+  if (countMenu === 0) {
+    countMenu = 1;
+    document.getElementById("menu").style.display = "grid";
+    document.getElementById("menu").style.position = "absolute";
+    document.getElementById("menu").style.backgroundColor = "rgb(8, 36, 90)";
+  } else {
+    countMenu = 0;
+    document.getElementById("menu").style.display = "none";
+  }
+}
+
+function openCityMenu(evt, cityName) {
+  document.getElementById("solution").style.display = "none";
+
   var i, x, tablinks;
   x = document.getElementsByClassName("city");
   for (i = 0; i < x.length; i++) {
@@ -23,5 +59,20 @@ function openCity(evt, cityName) {
     tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
   }
   document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.firstElementChild.className += " w3-border-red";
+  evt.currentTarget.firstElementChild.style;
+  if (countMenu === 0) {
+    countMenu = 1;
+    document.getElementById("menu").style.display = "grid";
+    document.getElementById("menu").style.position = "absolute";
+    document.getElementById("menu").style.backgroundColor = "rgb(8, 36, 90)";
+  } else {
+    countMenu = 0;
+    document.getElementById("menu").style.display = "none";
+  }
+
+
+}
+
+function showResult() {
+  document.getElementById("solution").style.display = "grid";
 }
