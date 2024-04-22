8534c29fb420060ae84c62d51c82cfb2af951c43
secigod <82354208+secigod@users.noreply.github.com>
2024-04-21 23:59:16 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,27 @@
 init("INCIO");
 var countMenu = 0;
 
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function openModal(event){
+  modal.style.display = "block";
+}
 
 function init(incio) {
   document.getElementById("solution").style.display = "none";
